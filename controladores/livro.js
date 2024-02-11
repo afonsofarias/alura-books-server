1d290790cbfdc974c73d51ece73d26fdc91a9003
Afonso Jansen de Mello Farias <afonsojansen@gmail.com>
2024-02-10 21:07:50 -0300

    getLivrosPorId

--- controladores/livro.js
+++ controladores/livro.js
@@ -1,6 +1,21 @@
+const fs = require("fs")
+const { getTodosLivros, getLivroPorId } = require("../servicos/livro")
+
 function getLivros(req, res) {
     try {
-        res.send('Olá mundo da Alura do Brasil!')
+        const livros = getTodosLivros()
+        res.send(livros)
+    } catch (error) {
+        res.status(500)
+        res.send(error.message)
+    }
+}
+
+function getLivro(req, res) {
+    try {
+        const id = req.params.id
+        const livro = getLivroPorId(id)
+        res.send(livro)
     } catch (error) {
         res.status(500)
         res.send(error.message)
@@ -8,5 +23,6 @@ function getLivros(req, res) {
 }
 
 module.exports = {
-    getLivros
+    getLivros,
+    getLivro
 }
