395d6090680e6130d94556d7ad421efd09e29cd7
Afonso Jansen de Mello Farias <afonsojansen@gmail.com>
2024-02-11 23:07:30 -0300

    .

--- app.js
+++ app.js
@@ -1,18 +1,12 @@
-const express = require("express")
-const rotaLivro = require("./rotas/livro")
-const rotaFavorito = require("./rotas/favorito")
-
-const cors = require("cors")
-
+const express = require('express')
+const routerLivros = require('./rotas/livros')
 const app = express()
-app.use(express.json())
-app.use(cors({origin: "*"}))
-
-app.use('/livros', rotaLivro)
-app.use('/favoritos', rotaFavorito)
-
 const port = 8000
 
+app.use(express.json())
+app.use('/livros', routerLivros)
+
+
 app.listen(port, () => {
-    console.log(`Escutando a porta ${port}`)
+  console.log(`Example app listening on port ${port}`)
 })
